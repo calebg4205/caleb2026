d7da39b94ece7bef8e377ed77de9d558b1520142
Caleb Garcia <calebjgarcia08@gmail.com>
2025-05-22 13:02:06 -0400

    Addinf Quality of Life Plan

--- frontend/my-app/src/App.jsx
+++ frontend/my-app/src/App.jsx
@@ -8,11 +8,21 @@ function App() {
     <>
       <h1>HSTAT can be comfterable, for <strong>everyone</strong></h1>
       <h3 class="meeting">Meet Caleb</h3>
-      <div>
+      <div id="list">
         <li>Hard-working</li>
         <l1>Problem-Solver</l1>
         <li>Experienced</li>
       </div>
+
+      <div class="qualityOfLife">
+        <h2>What is his Quality of Life plan?</h2>
+        <div>
+            <p>In short my quaiity of life plan will consist of three parts</p>
+        </div>
+        
+
+      </div>
+
 
     </>
   )
