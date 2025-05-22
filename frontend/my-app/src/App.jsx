9eeb395c16f623b2ef69c8754aecb2ab4226dce9
Caleb Garcia <calebjgarcia08@gmail.com>
2025-05-22 12:51:31 -0400

    New File: Creating backend and frontend

---
+++ frontend/my-app/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>HSTAT can be comfterable, for <strong>everyone</strong></h1>
+      <h3 class="meeting">Meet Caleb</h3>
+      <div>
+        <li>Hard-working</li>
+        <l1>Problem-Solver</l1>
+        <li>Experienced</li>
+      </div>
+
+    </>
+  )
+}
+
+export default App
